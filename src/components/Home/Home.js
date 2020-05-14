57c6c620d777b3ae9b8cb3102cdb093057d0e886
mtsai <megantsai920@gmail.com>
2020-05-14 09:41:08 -0400

    Updated CSS - 1. Updated CSS for Welcome page 2. Began working on a background image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = function () {
   return (
     <div className="homepage-div">
-      <h1>Welcome to</h1>
+      <h2>WELCOME TO</h2>
       <h1 className="homepage-brand">ALL BUN & GAMES</h1>
       <h3>Click on Create Recipe to get started, or view your current recipes.</h3>
       <h3 className="homepage-happy">Happy cooking!</h3>
